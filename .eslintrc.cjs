19d4a2eab653466635350ede1b1033a548424eaf
diomededavid <david@d2creativedesign.com>
2023-05-19 19:38:36 -0700

    adds airbnb eslint settings and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,14 +3,15 @@ module.exports = {
     browser: true,
     es2020: true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': 'warn'
+    'react-refresh/only-export-components': 'warn',
+    "prettier/prettier": "error"
   }
 };
